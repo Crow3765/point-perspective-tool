a7fd0be48f33e8205c6a8dbf8c70a86858e1cd7d
Crow3765 <corvusbazaldua@gmail.com>
2025-11-15 23:15:40 -0600

    Added basic pygame program code

--- src/project.py
+++ src/project.py
@@ -1,5 +1,24 @@
+import pygame
+
+
 def main():
-    pass
+    pygame.init()
+    pygame.display.set_caption('point perspective tool')
+    resolution = (1400, 800)
+    screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        # Program Event Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # Program Logic
+        # Render & Display
+        color = pygame.Color(255, 255, 255)
+        screen.fill(color)
+        pygame.display.flip()
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
