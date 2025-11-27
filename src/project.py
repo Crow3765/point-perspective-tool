23cd3aaf3ce1f83144cd96de27838b2df3014c4e
Crow3765 <corvusbazaldua@gmail.com>
2025-11-27 14:19:31 -0600

    Used an image for the vanishing point instead to save time

--- src/project.py
+++ src/project.py
@@ -5,23 +5,23 @@ class VanishingPoints():
 
     def __init__(self):
         self.color = pygame.Color(0, 0, 0)
-
-    def end(self, resolution, pos_x):
-        # Needs to extend to the edge of the canvas
-        angle = math.radians(45)
-        x = pos_x[0] + resolution[0] * math.cos(angle)
-        y = pos_x[1] + resolution[1] * math.sin(angle)
-        pos_y = (x, y)
-        return pos_y
-
-    def draw_rays(self, surface, pos_x, pos_y):
+        self.rays = self.point()
+    
+    def point(self):
+        # The image
+        point = pygame.image.load('assets/vanishing_point.png').convert()
+        return point
+    
+    def draw_ray(self, surface, pos_x):
         # Must activate when left mouse button is clicked
-        pygame.draw.line(surface, self.color, pos_x, pos_y, 3)
+        surface.blit(self.rays, pos_x)
 
 
 def main():
     pygame.init()
     pygame.display.set_caption('point perspective tool')
+    clock = pygame.time.Clock()
+    dt = 0
     resolution = (1400, 800)
     screen = pygame.display.set_mode(resolution)
     rays = VanishingPoints()
@@ -36,18 +36,19 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         # Program Logic
-        try:
-            pos_y = rays.end(resolution, pos_x)
-        except UnboundLocalError:
-            pass
+
         # Render & Display
         color = pygame.Color(255, 255, 255)
         screen.fill(color)
-        if pygame.mouse.get_pressed()[0] == True:
-            rays.draw_rays(screen, pos_x, pos_y)
+        # Keeps the image on the screen
+        try:
+            rays.draw_ray(screen, pos_x)
+        except UnboundLocalError:
+            pass
         pygame.display.flip()
+        dt = clock.tick(12)
     pygame.quit()
-
+# (951, 601)
 
 if __name__ == "__main__":
     main()
